819948501798717d2444a3957f69ffb83e0b0b66
kyledewy <15720036+kyledewy@users.noreply.github.com>
2021-03-26 15:13:10 -0700

    feat(feat: pcappedsmartpool. todo: pv2smartpool + experipie): 3 different pool implementations

--- test/PieDao.ts
+++ test/PieDao.ts
@@ -19,24 +19,21 @@ class SmartPoolBuilder {
   contract: PCappedSmartPool;
   controller?: string;
   supply?: BigNumber;
+  tokens?: string[];
+  name?: string;
 
   constructor(signer: Signer, contract: PCappedSmartPool) {
     this.contract = contract;
     this.signer = signer;
   }
 
-  async build(address: string) {
-    const pool = this.contract.attach(address);
-    // this.controller = await pool.connect(this.signer).getController();
-    const tokens = await pool.connect(this.signer).getTokens();
-    console.log('Tokens: ', tokens);
-    console.log('Controller: ', this.controller);
-    this.supply = await pool.connect(this.signer).totalSupply();
-  }
-
-  print() {
-    console.log('SmartPool');
-    console.log('Supply: ', this.supply?.toString());
+  async build(address: string): Promise<SmartPool> {
+    this.contract = this.contract.attach(address);
+    this.controller = await this.contract.connect(this.signer).getController();
+    this.tokens = await this.contract.connect(this.signer).getTokens();
+    this.supply = await this.contract.connect(this.signer).totalSupply();
+    this.name = await this.contract.connect(this.signer).name();
+    return new SmartPool(this.contract, this.controller, this.supply, this.tokens, this.name);
   }
 }
 
@@ -44,11 +41,25 @@ class SmartPool {
   contract: PCappedSmartPool;
   controller: string;
   supply: BigNumber;
+  tokens: string[];
+  name: string;
 
-  constructor(contract: PCappedSmartPool, controller: string, supply: BigNumber) {
+  constructor(contract: PCappedSmartPool, controller: string, supply: BigNumber, tokens: string[], name: string) {
     this.contract = contract;
     this.controller = controller;
     this.supply = supply;
+    this.tokens = tokens;
+    this.name = name;
+  }
+
+  print(implementation?: string) {
+    console.log("SmartPool: ", this.name);
+    console.log("  Address: ", this.contract.address);
+    if (implementation) console.log("  Implementation: ", implementation);
+    console.log("  Supply: ", this.supply?.toString());
+    console.log("  Tokens: ", this.tokens);
+    console.log("  Controller: ", this.controller);
+    console.log("");
   }
 }
 
@@ -65,14 +76,24 @@ describe("PieDao: Unit tests", function () {
     this.signers.admin = await new MainnetSigner(pieDaoAdmin).impersonateAccount();
 
     this.pools = [];
-    this.poolImplementation = new SmartPoolBuilder(this.signers.default, (await hre.ethers.getVerifiedContractAt("0xf13f977AaC9B001f155889b9EFa7A6628Fb7a181")) as PCappedSmartPool);
+    this.pV2SmartPool = await hre.ethers.getVerifiedContractAt("0x706F00ea85a71EB5d7C2ce2aD61DbBE62b616435");
+    this.PCappedSmartPool = (await hre.ethers.getVerifiedContractAt(
+      "0xf13f977AaC9B001f155889b9EFa7A6628Fb7a181",
+    )) as PCappedSmartPool;
 
     for (let i = 0; i < 6; i++) {
       const poolAddr = await this.smartPoolRegistry.connect(this.signers.default).entries(i);
       expect(await this.smartPoolRegistry.connect(this.signers.default).inRegistry(poolAddr)).to.eq(true);
-      const pool = await this.poolImplementation.build(poolAddr);
-      this.pools.push(pool);
-      this.poolImplementation.print();
+      const proxy = await hre.ethers.getVerifiedContractAt(poolAddr);
+      const implementation = await proxy.connect(this.signers.default).getImplementation();
+      if (implementation === "0xf13f977AaC9B001f155889b9EFa7A6628Fb7a181") {
+        const poolBuilder = new SmartPoolBuilder(this.signers.default, this.pV2SmartPool);
+        const pool = await poolBuilder.build(poolAddr);
+        this.pools.push(pool);
+        pool.print(implementation);
+      } else {
+        console.log("implementation ", implementation);
+      }
     }
   });
 
