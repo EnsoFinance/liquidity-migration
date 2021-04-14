796fb41033b64c15f00dee084540067d897e9f8e
kyledewy <15720036+kyledewy@users.noreply.github.com>
2021-04-13 19:09:20 -0700

    build(builder pattern): setup mainnet env for uniswap/balancer

--- src/enso.ts
+++ src/enso.ts
@@ -1,48 +1,46 @@
 const hre = require("hardhat");
-const { ethers, waffle } = hre;
 import * as constants from "./constants";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { deploy } from "@bodhi/contracts/";
-import { LiquidityMigration__factory, LiquidityMigration, IWETH } from "../typechain";
 import { BigNumber, constants as ethConstants, Contract } from "ethers";
+import { Builder } from "builder-pattern";
 
-export enum AcceptedProtocols {
-  Indexed,
-  DefiPulseIndex,
-  PieDao,
-}
-// =====================TODO: Move to Monorepo======================
-export type Options = {
+export class Options {
   threshold: number;
   slippage: number;
   numTokens: number;
   timelock: number;
   wethSupply: BigNumber;
-};
+  builder() {
+    return Builder<this>();
+  }
+  default(): Options {
+    return this.builder()
+      .threshold(10)
+      .slippage(995)
+      .numTokens(15)
+      .timelock(60)
+      .wethSupply(WETH_SUPPLY(15))
+      .build();
+  }
+}
 
 export const WETH_SUPPLY = (numTokens: number) => ethConstants.WeiPerEther.mul(100 * (numTokens - 1));
 
-export const DEFAULT_OPTIONS: Options = {
-  threshold: 10, // 10/1000 = 1%
-  slippage: 995, // 995/1000 = 99.5
-  numTokens: 15, // Number of erc20 tokens in environment + on uniswap
-  timelock: 60, // 1 minute
-  wethSupply: WETH_SUPPLY(15),
-};
-
 export class EnsoEnvironmentBuilder {
   signer: SignerWithAddress;
+  options: Options;
   enso?: deploy.Platform;
-  options?: Options;
   uniswap?: Contract;
   tokens?: Contract[];
   balancer?: Contract;
 
   constructor(signer: SignerWithAddress) {
     this.signer = signer;
+    this.options = new Options().default();
   }
-  async devnet(options?: Options) {
-    this.options = options === undefined ? DEFAULT_OPTIONS : options;
+
+  async buildLocal(options?: Options) {
     this.tokens = await deploy.deployTokens(this.signer, this.options.numTokens, this.options.wethSupply);
     this.uniswap = await deploy.deployUniswap(this.signer, this.tokens);
     this.enso = await deploy.deployPlatform(
@@ -50,13 +48,10 @@ export class EnsoEnvironmentBuilder {
       this.uniswap,
       new Contract(constants.WETH, this.tokens[0].abi, this.signer),
     );
-    // TODO:
   }
 
-  async mainnet(options?: Options) {
-    this.options = options === undefined ? DEFAULT_OPTIONS : options;
-
+  async buildMainnet(options?: Options) {
+    // TODO: get tokens
     // this.enso = await deploy.deployPlatform(this.signer, this.uniswap, new Contract(constants.WETH, this.tokens[0].abi, this.signer));
   }
 }
-
---
+++ src/mainnet.ts
@@ -0,0 +1,11 @@
+export type AaveTokenList = {
+    name: string,
+    decimals: number,
+    symbol: string, 
+    address: string,
+    chainId: number,
+    logoURI: string,
+    tags: string[]
+}
+
+const erc20s = () => JSON.parse('./tokenlist.json') as AaveTokenList[];
