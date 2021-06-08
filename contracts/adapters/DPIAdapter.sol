b638b673a3fae223c550b652773ae043058319df
amateur_dev <me@dipeshsukhani.dev>
2021-06-08 10:08:42 +0800

    refactored to call the execute  fx

--- contracts/adapters/DPIAdapter.sol
+++ contracts/adapters/DPIAdapter.sol
@@ -26,23 +26,7 @@ interface ISetBasicIsstanceModuleAddress {
 
 pragma solidity 0.8.2;
 
-// Logic of this Adapter Contract
-// MustHaves
-// 1. User will first have to approve this contract to use the client's DPI Tokens || Front-end task || out of purview of this contract {generic liquidity migration DIPESH TODO}
-// 2. User will then call a function that will transfer the DPI coins to this contract
-// 3. The contract will 'redeem' the DPI toknens
-// GoodToHaves
-// 4. First find out the components that will be released by the DPI Contract
-// 5. Check the balances already held for each of the components before calling the redeem function
-// 6. Check the balances held for each of the components after calling the redeem function
-// 7. Compute the difference between step 5 and step 6
-// Extended (to be discussed and to be completed)
-// 8. What to do with the underlying Tokens
-// 9. ERC20 Token issuance strategy against the DPI tokens
-
-
-
-/// @title DPI Vampire Attack Contract
+/// @title DPI Vampire Attack Contract for DPI Token
 /// @author Enso.finance (github.com/amateur-dev)
 /// @notice Adapter for redeeming the underlying assets from DPI 
 
@@ -51,26 +35,48 @@ contract DPIAdapter {
 
     // state variables
     ISetBasicIsstanceModuleAddress public setBasicIssuanceModule;
-    
+    mapping(address => uint) public whitelistedTokens;
+    address private manager;
     
     // events
     event RedemptionSuccessful();
 
+    // modifers
+    modifier onlyManager {
+        require (msg.sender == manager);
+        _;
+    }
 
     // constructor
-    constructor(ISetBasicIsstanceModuleAddress setBasicIssuanceModuleAddress) {
+    constructor(ISetBasicIsstanceModuleAddress setBasicIssuanceModuleAddress, addresss managerAddress) {
         setBasicIssuanceModule = setBasicIssuanceModuleAddress;
+        manager = managerAddress;
     } 
+
+    
+
     
     // readerFunctions
-    
+    function isInputToken(address token) public override view returns (bool) {
+            if (whitelistedTokens[token]==1) {
+                return true;
+            }
+            return false;
+        }    
     // executeableFunctions
 
     /// @notice Migrates the Token Set Contract's underlying assets under management
     /// @param tokenSetAddress is the address of the Token Set (eg DPI) which needs to be redeemed
     /// @param quantity of the coins that needs to be redeemed
     /// @param toWhom is the address to which all the underlying assets need to be sent at the time redemption
-    function migrateLiquidity(address tokenSetAddress, uint256 quantity, address toWhom) external returns (bool){
+
+    // TODO: working on this function call
+    function execute(bytes calldata inputData) external override {
+        (address tokenSetAddress, uint256 quantity, address toWhom) = abi.decode(inputData, (address, uint256, address));
+        require(migrateLiquidity(address tokenSetAddress, uint256 quantity, address toWhom));
+    }
+
+    function migrateLiquidity(address tokenSetAddress, uint256 quantity, address toWhom) internal returns (bool){
         IERC20(tokenSetAddress).transferFrom(msg.sender, address(this), quantity);
         address[] memory components = ISetToken(tokenSetAddress).getComponents();
         uint[] memory pre = new uint[](components.length);
@@ -91,9 +97,11 @@ contract DPIAdapter {
         }
         emit RedemptionSuccessful();
         return true;
-        //TODO: What to do after the tokens are transferred over
+
 
     // controllingFunctions
+    function addAcceptedTokensToWhitelist(address tokenAddress) onlyManager public returns (bool) {
+        whitelistedTokens(tokenAddress) = 1;
     }
 }
 
