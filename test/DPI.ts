8e886681976e4488f811bee6b555586a070d592f
amateur_dev <me@dipeshsukhani.dev>
2021-06-21 22:37:48 +0800

    getting the balances for all tokens for the users

--- test/DPI.ts
+++ test/DPI.ts
@@ -1,15 +1,17 @@
 import { ethers } from "hardhat";
+import { expect } from "chai";
 import bignumber from "bignumber.js"
 import { BigNumber, Contract, Event } from "ethers";
 import { Signers, MainnetSigner } from "../types";
 import { AcceptedProtocols, LiquidityMigrationBuilder } from "../src/liquiditymigration";
-import { IStrategy__factory } from "../typechain";
+import { ClashingImplementation, IERC20, IERC20__factory, IStrategy__factory } from "../typechain";
 
 
 import { DPIEnvironmentBuilder } from "../src/dpi";
 import { StrategyBuilder, Position } from "@enso/contracts";
 import { TASK_COMPILE_SOLIDITY_LOG_NOTHING_TO_COMPILE } from "hardhat/builtin-tasks/task-names";
 import { DIVISOR, THRESHOLD, TIMELOCK, SLIPPAGE } from "../src/constants";
+import { object } from "underscore";
 
 describe("DPI: Unit tests", function () {
   // lets create a strategy and then log its address and related stuff
@@ -83,35 +85,66 @@ describe("DPI: Unit tests", function () {
 
   it("Token holder should be able to withdraw from pool", async function () {
     // getting holders of DPI Tokens
-    const holderBalances = [];
+
+    const holderBalances: any[] = [];
     for (let i = 0; i < this.DPIEnv.holders.length; i++) {
       holderBalances[i] = {
         holder: await this.DPIEnv.holders[i].getAddress(),
-        balance: await this.DPIEnv.DPIToken.balanceOf((await this.DPIEnv.holders[i].getAddress()))
+        balance: await this.DPIEnv.DPIToken.balanceOf((await this.DPIEnv.holders[i].getAddress())),
       }
-      console.log((await this.DPIEnv.DPIToken.balanceOf((await this.DPIEnv.holders[i].getAddress()))).toString())
+      expect(await this.DPIEnv.DPIToken.balanceOf((await this.DPIEnv.holders[i].getAddress()))).to.gt(BigNumber.from(0));
     };
+
+    // getting the underlying tokens
+    const underlyingTokens = await this.DPIEnv.DPIToken.getComponents();
     
-    console.log(holderBalances);
-    // console.log(String(holderBalances[1].balance));
-    
-    // expect(adminBalance).to.eq(BigNumber.from(0));
+  // for (let index = 0; index < underlyingTokens.length; index++) {
+  //       const tokenContract = IERC20__factory.connect(underlyingTokens[index], this.signers.default) as IERC20;
+  //       const adds = tokenContract.address;
+  //       console.log(adds);
+  //       holderBalances.forEach(async (e)=>
+  //         {
+  //           e.underlyingTokenBalances = {}; // creating an empty object
+  //           const balance = await tokenContract.balanceOf(e.holder);
+  //           console.log(balance);
+  //           // adding key value pair to the empty object created
+  //           e.underlyingTokenBalances.abc =  "balance";
+  //         }
+  //       )
+  // }
 
-    // const holderBalance = await contract.balanceOf(await holder.getAddress());
-    // expect(holderBalance).to.be.gt(BigNumber.from(0));
+  // console.log(holderBalances);
 
-    // await expect(contract.connect(this.pieDaoEnv.admin).joinPool(100)).to.be.revertedWith(
-    //   "revert ERC777: transfer amount exceeds balance",
-    // );
-
-    // const totalSupply = await contract.totalSupply();
-
-    // const tokenBalances = [];
-    // for (let i = 0; i < pool.tokens.length; i++) {
-    //   const token = ERC20__factory.connect(pool.tokens[i], this.signers.default);
-    //   const balance = await token.balanceOf(await holder.getAddress());
-    //   tokenBalances.push(balance);
+    // for (let index = 0; index < holderBalances.length; index++) { // holderBalances.length = 3
+    //   for (let a = 0; a < underlyingTokens.length; a++) { // underlyingTokens.length = 14
+    //       const tokenContract = IERC20__factory.connect(underlyingTokens[a], this.signers.default) as IERC20;
+    //       const userBalanceOfUnderlyingToken = await tokenContract.balanceOf(holderBalances[index].holder);
+    //       holderBalances[index].underlyingTokenBalances = {
+    //         [tokenContract.address]: userBalanceOfUnderlyingToken
+    //       }
+    //   }
     // }
+
+
+    // getting the balance of each of the underlying tokens for the holders
+    // for (let i = 0; i < underlyingTokens.length; i++) {
+    //   // creating the contract
+    //   const tokenContract = IERC20__factory.connect(underlyingTokens[i], this.signers.default) as IERC20;
+    //   const tempAddress = tokenContract.address;
+    //   // console.log(`Getting value for ${tempAddress}`);
+    //   // getting balance of the user
+    //   for (let index = 0; index < this.DPIEnv.holders.length; index++) {
+    //     // console.log(index,`:`, await this.DPIEnv.holders[index].getAddress());
+    //     const underlyingTokenBalance = await tokenContract.balanceOf(await this.DPIEnv.holders[index].getAddress());
+    //     // console.log(tempAddress, ":", underlyingTokenBalance.toString());
+    //     // console.log(underlyingTokenBalance.toString());
+    //     holderBalances[index].underlyingTokenBalances = {
+    //       tempAddress: underlyingTokenBalance
+    //     }
+    //     // console.log(tempAddress,":", holderBalances[index].underlyingTokenBalances);
+    //   }
+    // }
+    
 
     // const tx = await contract.connect(holder).exitPool(holderBalance);
     // await tx.wait();
