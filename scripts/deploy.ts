e329adddf70c2d180d23f35e67980dd408f39ab4
PeterMPhillips <github@pmphillips.ca>
2021-10-06 10:55:35 -0700

    fix: deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,26 +6,30 @@
 import hre from 'hardhat'
 import { getBlockTime } from '../src/utils'
 
-const deployedContracts = {
+const deployedContracts : any = {
   mainnet: {
     genericRouter: '',
     strategyProxyFactory: '',
     strategyController: '',
-    tokenSetsBasicIssuanceModule: '0xd8EF3cACe8b4907117a45B0b125c68560532F94D'
+    tokenSetsBasicIssuanceModule: '0xd8EF3cACe8b4907117a45B0b125c68560532F94D',
+    tokenSetsDebtIssuanceModule: '0x39f024d621367c044bace2bf0fb15fb3612ecb92'
   },
   kovan: {
     genericRouter: '0xE0a9382c01d6EDfA0c933714b3626435EeF10811',
     strategyProxyFactory: '0xaF80BB1794B887de4a6816Ab0219692a21e8430e',
     strategyController: '0x077a70998D5086E6c6D53D9Fb7BCfd8F7fb73AC2',
-    tokenSetsBasicIssuanceModule: '0x8a070235a4B9b477655Bf4Eb65a1dB81051B3cC1'
+    tokenSetsBasicIssuanceModule: '0x8a070235a4B9b477655Bf4Eb65a1dB81051B3cC1',
+    tokenSetsDebtIssuanceModule: '0xe34031E7F4D8Ba4eFab190ce5f4D8451eD1B6A82'
   },
-  localhost: {
+  hardhat: {
     genericRouter: '0xE0a9382c01d6EDfA0c933714b3626435EeF10811',
     strategyProxyFactory: '0xaF80BB1794B887de4a6816Ab0219692a21e8430e',
     strategyController: '0x077a70998D5086E6c6D53D9Fb7BCfd8F7fb73AC2',
-    tokenSetsBasicIssuanceModule: '0x8a070235a4B9b477655Bf4Eb65a1dB81051B3cC1'
+    tokenSetsBasicIssuanceModule: '0x8a070235a4B9b477655Bf4Eb65a1dB81051B3cC1',
+    tokenSetsDebtIssuanceModule: '0x39f024d621367c044bace2bf0fb15fb3612ecb92'
   }
 }
+
 
 const owner = '0x0c58B57E2e0675eDcb2c7c0f713320763Fc9A77b'
 const initialURI = 'https://token-cdn-domain/{id}.json'
@@ -37,11 +41,13 @@ const state = [0, 1, 2] // 0 = pending, 1 = active, 2 = closed
 
 
 async function main() {
+  if (process.env.HARDHAT_NETWORK) {
     let protocol_addresses = [];
     const TokenSetAdapterFactory = await hre.ethers.getContractFactory("TokenSetAdapter")
     const tokenSetAdapter = await TokenSetAdapterFactory.deploy(
-        deployedContracts['localhost'].tokenSetsBasicIssuanceModule,
-        deployedContracts['localhost'].genericRouter,
+        deployedContracts[process.env.HARDHAT_NETWORK].tokenSetsBasicIssuanceModule,
+        deployedContracts[process.env.HARDHAT_NETWORK].tokenSetsDebtIssuanceModule,
+        deployedContracts[process.env.HARDHAT_NETWORK].genericRouter,
         owner
     )
     await tokenSetAdapter.deployed()
@@ -65,9 +71,9 @@ async function main() {
     const LiquidityMigrationFactory = await hre.ethers.getContractFactory("LiquidityMigration")
     const liquidityMigration = await LiquidityMigrationFactory.deploy(
       [tokenSetAdapter.address, BalancerAdapter.address, pieDaoAdapter.address],
-      deployedContracts['localhost'].genericRouter,
-      deployedContracts['localhost'].strategyProxyFactory,
-      deployedContracts['localhost'].strategyController,
+      deployedContracts[process.env.HARDHAT_NETWORK].genericRouter,
+      deployedContracts[process.env.HARDHAT_NETWORK].strategyProxyFactory,
+      deployedContracts[process.env.HARDHAT_NETWORK].strategyController,
       unlock,
       hre.ethers.constants.MaxUint256,
       owner
@@ -96,6 +102,9 @@ async function main() {
     console.log("Claimable:", claimable.address)
     await claimable.stateChange(state[1])
     console.log("State updated: Migrate all the competitors *evil laugh*")
+  } else {
+    console.log("Network undefined")
+  }
 }
 
 // We recommend this pattern to be able to use async/await everywhere
