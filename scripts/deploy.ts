da90f277cc94e23117bf5fa09b1c14fda19dbf4a
Peter Phillips <github@pmphillips.ca>
2021-10-12 16:57:01 -0700

    fix: correct adapters

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -16,71 +16,117 @@ const deployedContracts: any = {
     GenericRouter: "",
     StrategyProxyFactory: "",
     StrategyController: "",
+    Leverage2XAdapter: "",
     TokenSetsBasicIssuanceModule: "0xd8EF3cACe8b4907117a45B0b125c68560532F94D",
     TokenSetsDebtIssuanceModule: "0x39f024d621367c044bace2bf0fb15fb3612ecb92",
+    SUSD: "0x57ab1ec28d129707052df4df418d58a2d46d5f51"
   },
   kovan: {
     GenericRouter: "0xE0a9382c01d6EDfA0c933714b3626435EeF10811",
     StrategyProxyFactory: "0xaF80BB1794B887de4a6816Ab0219692a21e8430e",
     StrategyController: "0x077a70998D5086E6c6D53D9Fb7BCfd8F7fb73AC2",
+    Leverage2XAdapter: "",
     TokenSetsBasicIssuanceModule: "0x8a070235a4B9b477655Bf4Eb65a1dB81051B3cC1",
     TokenSetsDebtIssuanceModule: "0xe34031E7F4D8Ba4eFab190ce5f4D8451eD1B6A82",
+    SUSD: "0x57ab1ec28d129707052df4df418d58a2d46d5f51"
   },
   hardhat: {
-    GenericRouter: "0xE0a9382c01d6EDfA0c933714b3626435EeF10811",
-    StrategyProxyFactory: "0xaF80BB1794B887de4a6816Ab0219692a21e8430e",
-    StrategyController: "0x077a70998D5086E6c6D53D9Fb7BCfd8F7fb73AC2",
-    TokenSetsBasicIssuanceModule: "0x8a070235a4B9b477655Bf4Eb65a1dB81051B3cC1",
+    GenericRouter: "",
+    StrategyProxyFactory: "",
+    StrategyController: "",
+    Leverage2XAdapter: "",
+    TokenSetsBasicIssuanceModule: "0xd8EF3cACe8b4907117a45B0b125c68560532F94D",
     TokenSetsDebtIssuanceModule: "0x39f024d621367c044bace2bf0fb15fb3612ecb92",
+    SUSD: "0x57ab1ec28d129707052df4df418d58a2d46d5f51"
   },
   localhost: {
     GenericRouter: "",
     StrategyProxyFactory: "",
     StrategyController: "",
+    Leverage2XAdapter: "",
     TokenSetsBasicIssuanceModule: "0xd8EF3cACe8b4907117a45B0b125c68560532F94D",
     TokenSetsDebtIssuanceModule: "0x39f024d621367c044bace2bf0fb15fb3612ecb92",
+    SUSD: "0x57ab1ec28d129707052df4df418d58a2d46d5f51"
   },
 };
 
 const owner = "0x0c58B57E2e0675eDcb2c7c0f713320763Fc9A77b";
 const initialURI = "https://token-cdn-domain/{id}.json";
-const max = 3;
-const protocols = [0, 1, 2]; // 0 = TS, 1 = pie, 3 = indexed
+const max = 6;
 const supply = 1000;
-const state = [0, 1, 2]; // 0 = pending, 1 = active, 2 = closed
+
+enum PROTOCOLS {
+  TOKENSET,
+  PIEDAO,
+  INDEXED,
+  INDEXCOOP,
+  DHEDGE,
+  POWERPOOL
+}
+
+enum STATE {
+  PENDING,
+  ACTIVE,
+  CLOSED
+}
 
 async function main() {
   getMonorepoDeployments();
   if (network) {
     const protocol_addresses = [];
     const TokenSetAdapterFactory = await hre.ethers.getContractFactory("TokenSetAdapter");
+    const BalancerAdapterFactory = await hre.ethers.getContractFactory("BalancerAdapter");
+    const PieDaoAdapterFactory = await hre.ethers.getContractFactory("PieDaoAdapter");
+    const DHedgeAdapterFactory = await hre.ethers.getContractFactory("DHedgeAdapter");
+
     const tokenSetAdapter = await TokenSetAdapterFactory.deploy(
       deployedContracts[network].TokenSetsBasicIssuanceModule,
-      deployedContracts[network].TokenSetsDebtIssuanceModule,
+      deployedContracts[network].Leverage2XAdapter,
       deployedContracts[network].GenericRouter,
       owner,
     );
     await tokenSetAdapter.deployed();
     log("TokenSetAdapter", tokenSetAdapter.address);
-    protocol_addresses.push(tokenSetAdapter.address);
+    protocol_addresses[PROTOCOLS.TOKENSET] = tokenSetAdapter.address;
 
-    const BalancerAdapterFactory = await hre.ethers.getContractFactory("BalancerAdapter");
-    const BalancerAdapter = await BalancerAdapterFactory.deploy(owner);
-    await BalancerAdapter.deployed();
-    log("BalancerAdapter", BalancerAdapter.address);
-    protocol_addresses.push(BalancerAdapter.address);
-
-    const PieDaoAdapterFactory = await hre.ethers.getContractFactory("PieDaoAdapter");
     const pieDaoAdapter = await PieDaoAdapterFactory.deploy(owner);
     await pieDaoAdapter.deployed();
     log("PieDaoAdapter", pieDaoAdapter.address);
-    protocol_addresses.push(pieDaoAdapter.address);
+    protocol_addresses[PROTOCOLS.PIEDAO] = pieDaoAdapter.address;
+
+    const indexedAdapter = await BalancerAdapterFactory.deploy(owner);
+    await indexedAdapter.deployed();
+    log("IndexedAdapter", indexedAdapter.address);
+    protocol_addresses[PROTOCOLS.INDEXED] = indexedAdapter.address;
+
+    const indexCoopAdapter = await TokenSetAdapterFactory.deploy(
+      deployedContracts[network].TokenSetsBasicIssuanceModule,
+      deployedContracts[network].Leverage2XAdapter,
+      deployedContracts[network].GenericRouter,
+      owner,
+    );
+    await indexCoopAdapter.deployed();
+    log("IndexCoopAdapter", indexCoopAdapter.address);
+    protocol_addresses[PROTOCOLS.INDEXCOOP] = indexCoopAdapter.address;
+
+    const dHedgeAdapter = await DHedgeAdapterFactory.deploy(
+      owner,
+      deployedContracts[network].SUSD
+    );
+    await dHedgeAdapter.deployed();
+    log("DHedgeAdapter", dHedgeAdapter.address);
+    protocol_addresses[PROTOCOLS.DHEDGE] = dHedgeAdapter.address;
+
+    const powerPoolAdapter = await BalancerAdapterFactory.deploy(owner);
+    await powerPoolAdapter.deployed();
+    log("PowerPoolAdapter", powerPoolAdapter.address);
+    protocol_addresses[PROTOCOLS.POWERPOOL] = powerPoolAdapter.address;
 
     const unlock = await getBlockTime(60);
 
     const LiquidityMigrationFactory = await hre.ethers.getContractFactory("LiquidityMigration");
     const liquidityMigration = await LiquidityMigrationFactory.deploy(
-      [tokenSetAdapter.address, BalancerAdapter.address, pieDaoAdapter.address],
+      protocol_addresses,
       deployedContracts[network].GenericRouter,
       deployedContracts[network].StrategyProxyFactory,
       deployedContracts[network].StrategyController,
@@ -100,7 +146,7 @@ async function main() {
       await erc1155.create(claimable.address, supply, initialURI, "0x");
     }
     log("Claimable", claimable.address);
-    await claimable.stateChange(state[1]);
+    await claimable.stateChange(STATE.ACTIVE);
     console.log("State updated: Migrate all the competitors *evil laugh*");
     write2File();
   } else {
