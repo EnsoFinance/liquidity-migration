8425e3e0791999f44c3bab4cbeda971a794ad9ef
Joe Pegler <joepegler123@gmail.com>
2021-12-09 14:23:15 +0000

    change ethereum whale

--- tasks/addOwnerFunds.ts
+++ tasks/addOwnerFunds.ts
@@ -1,7 +1,7 @@
 import { task } from "hardhat/config";
 import { ADD_OWNER_FUNDS } from "./task-names";
 export const owner = "0x0c58B57E2e0675eDcb2c7c0f713320763Fc9A77b";
-const ethereumHolder = "0xAb5801a7D398351b8bE11C439e05C5B3259aeC9B";
+const ethereumHolder = "0x9bf4001d307dfd62b26a2f1307ee0c0307632d59";
 
 task(ADD_OWNER_FUNDS, "Add Owner Funds", async (_taskArgs, hre) => {
   await hre.network.provider.request({
