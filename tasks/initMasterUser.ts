715cd95b92305e51b5341172d590b4aa9ca24b41
Peter Phillips <github@pmphillips.ca>
2021-12-09 07:35:13 -0800

    fix: remove MVI from TokenSets

--- tasks/initMasterUser.ts
+++ tasks/initMasterUser.ts
@@ -172,14 +172,6 @@ export const LP_TOKEN_WHALES = [
   },
   {
     victim: "tokenSets",
-    walletAddress: "0xBAdb34560712bf98c93E168Bf9343fb627162eDd",
-    lpTokenAddress: "0x72e364F2ABdC788b7E918bc238B21f109Cd634D7",
-    lpTokenName: "Metaverse Index",
-    symbol: "MVI",
-    adapter: "TokenSetAdapter",
-  },
-  {
-    victim: "tokenSets",
     walletAddress: "0xaD0C123B819C6c137ed2Cdd009B546cFd7445C2e",
     lpTokenAddress: "0xa188DA64fc4e212Cda65bD3406e0ce03a5323560",
     lpTokenName: "Tipuana Genesis Fund",
