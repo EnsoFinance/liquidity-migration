a3b8e8b877ebffc381284c12050e7b09fc149fa2
PeterMPhillips <github@pmphillips.ca>
2021-12-09 14:05:25 -0800

    feat: mainnet deploy script

--- scripts/deploy2MainnetFork.sh
+++ scripts/deploy2MainnetFork.sh
@@ -7,3 +7,4 @@ yarn deploy localhost
 yarn hardhat initMasterUser --network localhost
 yarn hardhat addAllAdapters --network localhost
 yarn hardhat whitelistAllStrategies --network localhost
+yarn hardhat transferOwnership --to '0xEE0e85c384F7370FF3eb551E92A71A4AFc1B259F' --network localhost
